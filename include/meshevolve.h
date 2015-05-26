4a4d97dd7c2cc4616806694337e6a1cc7e5259c1
Julien Daval <julien.daval@ensimag.grenoble-inp.fr>
2015-05-26 16:52:21 +0200

    Début de l'évolution du maillage

--- include/meshevolve.h
+++ include/meshevolve.h
@@ -1,11 +1,27 @@
-#ifndef MESHEVOLVE_H
-#define MESHEVOLVE_H
+#ifndef DEF_MESHEVOLVE
+#define DEF_MESHEVOLVE
 
+#include "mesh.h"
+#include "skeleton.h"
+#include "sphere.h"
+
+typedef OpenMesh::PolyMesh_ArrayKernelT<> BMesh;
 
 class MeshEvolve
 {
+
 public:
-    MeshEvolve();
+    MeshEvolve(Mesh &mesh, Skeleton &s);
+
+    bool evolve();
+
+private:
+    BMesh m;
+    Skeleton s;
+
+    OpenMesh::Vec3f scalarNormal(const OpenMesh::Vec3f &p);
+    float fi(const Sphere *s, const OpenMesh::Vec3f &p);
+    float scalarField(const OpenMesh::Vec3f &p);
 };
 
-#endif // MESHEVOLVE_H
+#endif
