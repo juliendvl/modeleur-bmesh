f4f515ebaab473440531f6d35b8643dcee898119
Julien Daval <julien.daval@ensimag.grenoble-inp.fr>
2015-05-28 14:51:55 +0200

    Modif dans OMEigen
    Ajout de deux méthodes statiques pour aussi faire la conversion
    Vector2f <--> Vec2f

--- include/omeigen.h
+++ include/omeigen.h
@@ -9,8 +9,16 @@ class OMEigen
 {
 
 public:
+    static Eigen::Vector2f toEigen(const OpenMesh::Vec2f &p) {
+        return Eigen::Vector2f(p[0], p[1]);
+    }
+
     static Eigen::Vector3f toEigen(const OpenMesh::Vec3f &p) {
         return Eigen::Vector3f(p[0], p[1], p[2]);
+    }
+
+    static OpenMesh::Vec2f toOpenMesh(const Eigen::Vector2f &p) {
+        return OpenMesh::Vec2f(p[0], p[1]);
     }
 
     static OpenMesh::Vec3f toOpenMesh(const Eigen::Vector3f &p) {
